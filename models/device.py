c281aec892db9b4f1cd994ba9242e179868e25f4
franciscoabenza <fran.abenza@gmail.com>
2022-06-01 16:22:05 +0200

    another try

--- models/device.py
+++ models/device.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from typing import List, Optional
-import json
+from unicodedata import category
 
 from sqlmodel import Field, Relationship, SQLModel, create_engine, Enum
 
@@ -9,24 +9,28 @@ class Device(SQLModel, table=True):
     location: Optional[str] = None
 
     satisfactions: List["Satisfaction"] = Relationship(back_populates="device")
+    
 
 class Satisfaction(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     satisfaction: str
     insertedAt: datetime
-
+    location: str = None
+    category: Optional[str] = None
     deviceId: str = Field(foreign_key="device.deviceId")
     device: Device = Relationship(back_populates="satisfactions")
     def toJSON(self):
         return {
             "satisfaction": self.satisfaction,
         }
+class UpdateLocation(SQLModel, table=False):
+    deviceId: str = Field(primary_key=True)
+    location: Optional[str] = None
 
 
-db_url = 'mysql+pymysql://root:mypass@localhost:3306/db'
+db_url = 'mysql+pymysql://frans-mbp:Plesken2@localhost:3306/project3'
 
 engine = create_engine(db_url, echo=True)
-
 
 
 def create_db_and_tables():
